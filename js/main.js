910e4a8ab26b488bc8219c76d3a4698215b5f2f2
Omar Torres Urrego <omarurrego@192.168.0.3>
2023-06-22 23:20:56 -0500

    added animated library

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
 $(document).ready(function () {
-  $("#test").on("click", function () {
-    alert("hola");
-  });
   $(window).scroll(function (event) {
     var scroll = $(window).scrollTop();
     $(".navbar").addClass('navbar_bg');
   });
+
+  $(window).on("scroll", () => {
+    $("[touching]").each(function() {
+      var className = $('.services__sec').attr("touching")
+      var offset = $('.services__sec').offset().top - $(window).scrollTop();
+      if (offset <= 0) {
+        $('.services__sec').addClass("animate__animated animate__fadeInLeft")
+      } else {
+        $('.services__sec').removeClass("animate__animated animate__fadeInLeft")
+      }
+    })
+  }).trigger("scroll")
+
 });
