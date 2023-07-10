9205533c4f47e2ecb4224511325c305039a91d94
Omar Torres Urrego <omarurrego@192.168.0.3>
2023-07-09 23:56:13 -0500

    version 2.0 ingeorbe website

--- js/main.js
+++ js/main.js
@@ -1,14 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
+
+
   // FUNCTION TO ADD CLASS TO THE NAVBAR WHEN IT SCROLL
   window.addEventListener('scroll', () => {
     document.querySelector('.navbar').classList.add('navbar_bg')
   })
   // FUNCTION TO REMOVE CLASS TO THE NAVBAR WHEN GETS BACK TOP
   window.addEventListener('scroll', () => {
-    let scrollPosition = window.scrollY;
+    let scrollPosition = window.scrollY
     if (scrollPosition === 0) {
       document.querySelector('.navbar').classList.remove('navbar_bg')
       document.querySelector('.navbar').classList.add('navbar')
     }
   })
-});
+
+  
+  
+})
+
+function scrollToSection(sectionId) {
+  let section = document.getElementById(sectionId);
+  section.scrollIntoView({ behavior: 'smooth' });
+}
