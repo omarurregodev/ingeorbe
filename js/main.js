367aabd131dc81539de6d7828264daeb4215449b
Omar Torres Urrego <omarurrego@192.168.0.7>
2023-07-13 23:14:09 -0500

    new functionalities, and new version of the website

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
+  // DELAY UVP AND BLOCKQUOTE ANIMATION
 
+  setTimeout(() => {
+    document.querySelector('.hero').classList.add('animate__fadeInLeft');
+    document.querySelector('.hero').classList.remove('hidden');
+    document.querySelector('.blockquote').classList.add('animate__fadeInLeft');
+    document.querySelector('.blockquote').classList.remove('hidden');
+    document.querySelector('.blockquote-footer').classList.add('animate__fadeInRight');
+    document.querySelector('.blockquote-footer').classList.remove('hidden');
+    document.querySelector('.img_hero').classList.add('animate__fadeInRight');
+    document.querySelector('.img_hero').classList.remove('hidden');
+    document.querySelector('.figure_slider').classList.add('fade-in');
+  }, 1000);
 
   // FUNCTION TO ADD CLASS TO THE NAVBAR WHEN IT SCROLL
   window.addEventListener('scroll', () => {
